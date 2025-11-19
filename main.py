36a1d8678d2431f95fcdd139dc9ba408c68df18d
cristianism0 <csg2099@gmail.com>
2025-11-18 21:23:02 -0300

    add: backup func

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
-import os
+import logging
 from pathlib import Path
 
-PATH = Path('.')                                           # Select the PATH, use a '.' if you're already on it.
-SUB_dir = [sub for sub in PATH.iterdir() if sub.is_dir()]  # Returns all subdirectories in PATH
+# Select the PATH, use a '.' if you're already on it.
+PATH = Path('.')
 
-def sub_dir_join(SUB_dir_list = SUB_dir):
-    for sub_directory in SUB_dir_list:
+# Do you want to backup all your files on a BACKUP directory?
+# YES -> True (default)
+# NO -> False
+BACKUP = True
+
+# If you want to exclude a directory from being touched, move it to backup:
+# Secret directories (starts with a .) will be ignored by default.
+EXCLUDE_DIRS = ["BACKUP"]
+
+def backup_dir(MD_files: list, choice: bool = BACKUP):
+    """Create a BACKUP directory on the PATH.""" 
+    if choice:
+        backup_path = Path('BACKUP')
+        backup_path.mkdir(parents = True, exist_ok = True)
+        for files in MD_files:
+            files.copy_into('BACKUP', preserve_metadata = True)
+    else:
+        print("Backup folder was not created.")
 
 
+def collect_dirs_md(PATH: object = PATH):
+    """Collect all subdirectories from PATH and all md files on each of it."""
 
+    # Collects all child names for visualization
+    CHILD_NAMES = [child.name for child in PATH.iterdir() if child.is_dir()]
+
+    logging.warning("Directories accessed: %s", CHILD_NAMES)
+    print("\nIf you want to remove any of it, put on EXCLUDE_DIR.")
+
+    # Iterate over all directories and find .md files:
+    md_files = list(PATH.rglob('**.md'))
+
+    return md_files
+
+def meta_data_reader():
+    """Read all meta data from the folders."""
+    return 0
 
